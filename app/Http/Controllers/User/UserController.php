e1f1cacc8b58c752880d6bd3cb08b8554727f351
unknown <akinsuyi.joshua84@gmail.com>
2019-08-27 21:40:57 -0700

    first commit

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+         $this->middleware('permission:user-list');
+         $this->middleware('permission:user-create', ['only' => ['create','store']]);
+         $this->middleware('permission:user-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:user-delete', ['only' => ['destroy']]);
+    }
+    
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(
+            [
+                'status' => 'success',
+                'users' => $users->toArray()
+            ], 200);
+    }
+    public function show(Request $request, $id)
+    {
+        $user = User::find($id);
+        return response()->json(
+            [
+                'status' => 'success',
+                'user' => $user->toArray()
+            ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', 'Auth\AuthController@register');
+    Route::post('login', 'Auth\AuthController@login');
+    Route::get('refresh', 'Auth\AuthController@refresh');
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('user', 'Auth\AuthController@user');
+        Route::post('logout', 'Auth\AuthController@logout');
+    });
+});
+Route::group(['middleware' => 'auth:api'], function(){
+    // Users
+    Route::get('users', 'User\UserController@index');
+    Route::get('users/{id}', 'User\UserController@show');
+});
